6f17df8dbf5cdc60e515ce62cca12116dbe21fe4
margaretnelson <77342799+margaretnelson@users.noreply.github.com>
2021-02-02 11:22:03 -0500

    Add files via upload

---
+++ HW2.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[12]:
+
+
+colorList = ["teal","yellow", "periwinkle"]
+for color in colorList:
+    print(color)
+
+
+# In[13]:
+
+
+n = 0
+numbers = list(range(10))
+for n in numbers:
+    print(n)
+
+
+# In[8]:
+
+
+n = 0
+while n < 10:
+    print(n)
+    n = n + 1
+
+
+# In[9]:
+
+
+n = 0
+while n <= 10:
+    print(n)
+    n = n + 1
+else:
+    print("greater than 10")
+
+
+# In[10]:
+
+
+n = 30
+sum = 0
+i = 1
+
+while i <= n:
+    sum = sum + i
+    i = i+1
+    
+print(sum)
+
+
+# In[ ]:
+
+
+
+
